45bd8f31fd18e27408eecffc4a8ac5185424933e
Thomas Matlock <thomasmatlockbba@gmail.com>
2023-03-31 23:10:26 -0500

    Create MovitivaionApp.cs

---
+++ MovitivaionApp.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+
+        // Display title 
+        Console.WriteLine("Welcome to Our Landing Page!");
+
+        // Display header 
+        Console.WriteLine("------------------------");
+        Console.WriteLine();
+
+        // Display information about the company 
+        Console.WriteLine("Our Company:");
+        Console.WriteLine("Founded in 2021, we are a startup company dedicated to providing innovative solutions for businesses around the world.");
+        Console.WriteLine();
+
+        // Display information about our products/services 
+        Console.WriteLine("Our Products/Services:");
+        Console.WriteLine("- Product A: a revolutionary software tool for automating business processes");
+        Console.WriteLine("- Service B: consulting services for businesses looking to optimize their operations");
+        Console.WriteLine();
+
+        // Display contact information 
+        Console.WriteLine("Contact Us:");
+        Console.WriteLine("- Phone: 555-1234");
+        Console.WriteLine("- Email: info@ourcompany.com");
+        Console.WriteLine("- Address: 123 Main Street, Anytown, USA");
+        Console.WriteLine();
+        
+        // Display footer 
+        Console.WriteLine("------------------------");
+        Console.WriteLine("Thank you for visiting!");
+        
+        // Wait for user input before closing 
+        Console.ReadLine();
+    }
+}
